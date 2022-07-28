76aded3bba7267406e14a2e9cc2944a4b8e8c152
Lucas Colombo <colombo.lucas@outlook.com>
2022-07-28 15:14:55 -0300

    mongoose fix

--- config/db.config.js
+++ config/db.config.js
@@ -2,12 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect() {
 	try {
-		const dbConnection = await mongoose.connect(process.env.MONGODB_URI, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			useCreateIndex: true,
-			useFindAndModify: false,
-		});
+		const dbConnection = await mongoose.connect(process.env.MONGODB_URI);
 
 		console.log('Connect to DB:', dbConnection.connections[0].name);
 	} catch (err) {
