69af321a5c732bfefdbebf7bdb13e21c3f200220
Lucas Colombo <colombo.lucas@outlook.com>
2022-08-16 19:33:04 -0300

    user model condition

--- models/User.model.js
+++ models/User.model.js
@@ -1,18 +1,19 @@
 const { Schema, model, default: mongoose } = require('mongoose');
 
 const userSchema = new Schema({
-	name: { type: String, required: true, trim: true, match: /\s/gm },
-	email: {
-		type: String,
-		required: true,
-		unique: true,
-		trim: true,
-		match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-	},
-	passwordHash: { type: String, required: true },
-	flight: [{ type: mongoose.Types.ObjectId, ref: 'Flight' }],
-	aircraft: [{ type: mongoose.Types.ObjectId, ref: 'Aircraft' }],
-	isActive: { type: Boolean, default: true },
+  firstName: { type: String, required: true, trim: true },
+  lastName: { type: String, required: true, trim: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+  },
+  passwordHash: { type: String, required: true },
+  flight: [{ type: mongoose.Types.ObjectId, ref: 'Flight' }],
+  aircraft: [{ type: mongoose.Types.ObjectId, ref: 'Aircraft' }],
+  isActive: { type: Boolean, default: true },
 });
 
 const UserModel = model('User', userSchema);
